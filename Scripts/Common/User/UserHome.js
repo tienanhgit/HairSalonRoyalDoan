7547dbe61194d14ce429806deb0b5f690f596af6
vas <vas@DESKTOP-C5LNGFV>
2020-09-29 13:30:47 +0700

    commit lan 2

---
+++ Scripts/Common/User/UserHome.js
@@ -0,0 +1,13 @@
+﻿
+var swiper = new Swiper('.swiper-container', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false
+
+    }
+   
+});
