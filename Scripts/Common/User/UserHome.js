7a5b53f25183cff418d2aa9edbe3834c8b6e66ce
vas <buitienanh1998>
2020-09-30 16:56:02 +0700

    up load buoi chieu

--- Scripts/Common/User/UserHome.js
+++ Scripts/Common/User/UserHome.js
@@ -1,5 +1,5 @@
 ﻿
-var swiper = new Swiper('.swiper-container', {
+var swiper = new Swiper('.swp1', {
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
@@ -10,6 +10,30 @@ var swiper = new Swiper('.swiper-container', {
 
     }
    
+});
+//swiper 2
+var swiper2 = new Swiper('.swiper2', {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    // init: false,
+    pagination: {
+        el: '.swp2',
+        clickable: true,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 5,
+            spaceBetween: 50,
+        },
+    }
 });
 
 
