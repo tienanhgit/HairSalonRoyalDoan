0db561d14b169ead826bfa079c1e3323cea7ea17
vas <buitienanh1998>
2020-10-01 17:34:30 +0700

    sdfdsf

--- Controllers/User/UserProductController.cs
+++ Controllers/User/UserProductController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using HairSalonRoyalDoan.Models.Dictionary;
+using RoyalDoanEnitty.Dictionary;
+
 namespace HairSalonRoyalDoan.Controllers
 {
     public class UserProductController : Controller
@@ -11,6 +14,14 @@ namespace HairSalonRoyalDoan.Controllers
         // GET: UserProduct
         public ActionResult Index()
         {
+            SanPham sanPham = new SanPham();
+            ProductModel productModel = new ProductModel();
+            List<SanPham> sanpham = productModel.GetData();
+        
+                ViewBag['a'] = sanpham[0].HinhAnh;
+
+        
+
             return View();
         }
     }
