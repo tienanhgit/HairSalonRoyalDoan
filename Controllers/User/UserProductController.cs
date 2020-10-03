ff821f6f7163236865ff7212d6ea8eae1ebed34a
Tien Anh Bui <minhanhtienanh@gmail.com>
2020-10-03 19:12:19 +0700

    fix err

--- Controllers/User/UserProductController.cs
+++ Controllers/User/UserProductController.cs
@@ -5,20 +5,22 @@ using System.Web;
 using System.Web.Mvc;
 
 using HairSalonRoyalDoan.Models.Dictionary;
-using RoyalDoanEnitty.Dictionary;
+
 
 namespace HairSalonRoyalDoan.Controllers
 {
     public class UserProductController : Controller
     {
-        // GET: UserProduct
+
         public ActionResult Index()
         {
 
-            ProductModel productModel = new ProductModel();
-            List<SanPham> dssanpham = productModel.GetData();
-               
-            return View(dssanpham);
+            //        ProductModel productModel = new ProductModel();
+            //        //List<SanPham> dssanpham = productModel.GetData();
+
+            return View();
+            //    }
+            //}
         }
     }
 }
--- Models/Dictionary/ProductModel.cs
+++ Models/Dictionary/ProductModel.cs
@@ -5,47 +5,47 @@ using System.Linq;
 using System.Web;
 using System.Text;
 using HairSalonRoyalDoan.Models.Common;
-using RoyalDoanEnitty.Dictionary;
+
 
 namespace HairSalonRoyalDoan.Models.Dictionary
 {
     public class ProductModel
     {
 
-        DataProvider dataProvider = new DataProvider();
-        public List<SanPham> GetData()
-        {
-            try
-            {
-                List<SanPham> dsSanPham = new List<SanPham>();
-                DataTable dt = dataProvider.ExecuteQuery("Proc_SanPham_GetData1", null, null);
-                if (dt != null && dt.Rows.Count > 0)
-                {
-                    foreach (DataRow row in dt.Rows)
-                    {
-                        SanPham sanpham = new SanPham();
-                      sanpham.MaSanPham = String.IsNullOrEmpty(row["MaSanPham"].ToString()) ? 0 : int.Parse(row["MaSanPham"].ToString());
-                        sanpham.MaDanhMuc = String.IsNullOrEmpty(row["MaDanhMuc"].ToString()) ? 0 : int.Parse(row["MaDanhMuc"].ToString());
-                        sanpham.TenSanPham = String.IsNullOrEmpty(row["TenSanPham"].ToString()) ? "" : row["TenSanPham"].ToString();
-                        sanpham.HinhAnh= String.IsNullOrEmpty(row["HinhAnh"].ToString()) ? "" : row["HinhAnh"].ToString();
-                        sanpham.Gia = String.IsNullOrEmpty(row["Gia"].ToString()) ? 0 : float.Parse(row["Gia"].ToString());
-                        sanpham.MoTa = String.IsNullOrEmpty(row["MoTa"].ToString()) ? "" : row["MoTa"].ToString();
-                        sanpham.DanhGia = String.IsNullOrEmpty(row["DanhGia"].ToString()) ? "" : row["DanhGia"].ToString();
+        //DataProvider dataProvider = new DataProvider();
+        //public List<SanPham> GetData()
+        //{
+        //    try
+        //    {
+        //        List<SanPham> dsSanPham = new List<SanPham>();
+        //        DataTable dt = dataProvider.ExecuteQuery("Proc_SanPham_GetData1", null, null);
+        //        if (dt != null && dt.Rows.Count > 0)
+        //        {
+        //            foreach (DataRow row in dt.Rows)
+        //            {
+        //                SanPham sanpham = new SanPham();
+        //              sanpham.MaSanPham = String.IsNullOrEmpty(row["MaSanPham"].ToString()) ? 0 : int.Parse(row["MaSanPham"].ToString());
+        //                sanpham.MaDanhMuc = String.IsNullOrEmpty(row["MaDanhMuc"].ToString()) ? 0 : int.Parse(row["MaDanhMuc"].ToString());
+        //                sanpham.TenSanPham = String.IsNullOrEmpty(row["TenSanPham"].ToString()) ? "" : row["TenSanPham"].ToString();
+        //                sanpham.HinhAnh= String.IsNullOrEmpty(row["HinhAnh"].ToString()) ? "" : row["HinhAnh"].ToString();
+        //                sanpham.Gia = String.IsNullOrEmpty(row["Gia"].ToString()) ? 0 : float.Parse(row["Gia"].ToString());
+        //                sanpham.MoTa = String.IsNullOrEmpty(row["MoTa"].ToString()) ? "" : row["MoTa"].ToString();
+        //                sanpham.DanhGia = String.IsNullOrEmpty(row["DanhGia"].ToString()) ? "" : row["DanhGia"].ToString();
 
 
-                        dsSanPham.Add(sanpham);
-                    }
-                    return dsSanPham;
-                }
-                return new List<SanPham>();
-            }
-            catch (Exception ex)
-            {
-                return new List<SanPham>();
-            }
+        //                dsSanPham.Add(sanpham);
+        //            }
+        //            return dsSanPham;
+        //        }
+        //        return new List<SanPham>();
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return new List<SanPham>();
+        //    }
 
 
-        }
+        //}
 
 
 
