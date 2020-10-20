53f80fa366bd68b6bb81474da8944e73b32048a7
vas <buitienanh1998>
2020-10-20 11:32:31 +0700

    ok

--- Controllers/Common/SessionHelper.cs
+++ Controllers/Common/SessionHelper.cs
@@ -10,7 +10,7 @@ namespace HairSalonRoyalDoan.Controllers.Common
     public class SessionHelper
     {
         public static string USER_SESSION = "USER_SESSION";
-        public static string CartSession = "CartSession";
+        public static string CART_SESSION = "CART_SESSION";
 
 
     }
---
+++ Controllers/User/GioHangController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HairSalonRoyalDoan.Controllers;
+using HairSalonRoyalDoan.Models.Dictionary;
+using HairSalonRoyalDoan.Repository;
+
+namespace HairSalonRoyalDoan.Controllers.User
+{
+    public class GioHangController : Controller
+    {
+        
+        // GET: GioHang
+        public ActionResult Index()
+        {
+            var cart = Session["CART_SESSION"];
+            var list = new List<GioHangItem>();
+            if (cart != null)
+            {
+                list = (List<GioHangItem>)cart;
+            }
+        
+            return View(list);
+        }
+
+        [HttpPost]
+        public ActionResult ThemSanPhamGioHang(int SanPhamID)
+        {
+            var message = ""; 
+            var Soluong = 1;
+            var sanpham = new ProductModel().GetSanPhamByMa(SanPhamID);
+            var cart = Session["CART_SESSION"];
+            if (cart != null)
+            {
+                var list = (List<GioHangItem>)cart;
+                if (list.Exists(x => x.sanpham.MaSanPham == SanPhamID))
+                {
+                    foreach (var item in list)
+                    {
+                        if (item.sanpham.MaSanPham == SanPhamID)
+                        {
+                            item.SoLuong += Soluong;
+                        }
+                    }
+                }
+                else
+                {
+                  
+                var item = new GioHangItem();
+                item.sanpham = sanpham;
+                item.SoLuong = Soluong;
+                list.Add(item);
+                Session["CART_SESSION"] = list;
+                }
+            }
+            else
+            {
+                //tạo mới đối tượng cart item
+                var item = new GioHangItem();
+                item.sanpham = sanpham;
+                item.SoLuong = Soluong;
+                var list = new List<GioHangItem>();
+                list.Add(item);
+                //Gán vào session
+                Session["CART_SESSION"] = list;
+
+
+            }
+            return Json(new { Message = message, JsonRequestBehavior.AllowGet });
+
+        }
+
+
+
+
+    }
+}
